c6c7cc72642974dc925a7f048508bb6b20324306
Pablo Diaz Ogni <pdiazogni-contractor@cars.com>
2023-11-29 12:35:31 -0300

    Refactor integration test to reuse setUp in other tests

---
+++ tests/integration/__init__.py
@@ -0,0 +1,74 @@
+from webtest import TestApp
+import unittest
+
+import alembic
+import alembic.config
+import alembic.command
+import transaction
+from pyramid.paster import get_appsettings
+
+from leads_api import main
+from leads_api.models import get_tm_session, get_engine
+from leads_api.models.meta import metadata as dbmetadata
+
+
+class BaseIntegrationTest(unittest.TestCase):
+    """
+    Base integration TestCase with everything needed to fully test
+    the API responses while also using the SQLAlchemy model, making
+    sure everything is cleanup after test runs.
+
+    Notice that we are using `addCleanup()` over `tearDown()` to make sure
+    it's always called even if `setUp()` fails
+    """
+
+    def setUp(self):
+        # Settings for both API config (with db credentials) and for Alembic
+        # to create the database model needed for tests
+        ini_file = 'testing.ini'
+        settings = get_appsettings(ini_file)
+        alembic_cfg = alembic.config.Config(ini_file)
+
+        # Create a DB engine based on the settings
+        dbengine = get_engine(settings)
+
+        # Make sure to cleanup the database before and after running tests
+        def dbcleanup():
+            dbmetadata.drop_all(bind=dbengine)
+            alembic.command.stamp(alembic_cfg, None, purge=True)
+
+        dbcleanup()
+        self.addCleanup(dbcleanup)
+
+        # Run alembic migrations to create a fresh schema
+        alembic.command.upgrade(alembic_cfg, "head")
+
+        # Creates the Pyramid application with the settings and the dbengine
+        app = main({}, dbengine=dbengine, **settings)
+
+        # Configure a transaction manager to ensure the DB session is rollbacked
+        # after each test
+        self.tm = transaction.manager
+        self.tm.begin()
+        self.tm.doom()
+        self.addCleanup(self.tm.abort)
+
+        # Create a DB session hooked to the transaction manager,
+        # to make sure changes are always rollbacked
+        self.dbsession = get_tm_session(
+            app.registry['dbsession_factory'],
+            self.tm
+        )
+
+        # Create the TestApp used for making the requests to test
+        # the endpoints:
+        # response = self.testapp.get('GET', '/endpoint')
+        self.testapp = TestApp(
+            app,
+            extra_environ={
+                'HTTP_HOST': 'leads_api.com',
+                'tm.active': True,
+                'tm.manager': self.tm,
+                'app.dbsession': self.dbsession,
+            },
+        )
--- tests/integration/views/test_coverage.py
+++ tests/integration/views/test_coverage.py
@@ -1,55 +1,10 @@
-from webtest import TestApp
-import unittest
-
-import alembic
-import alembic.config
-import alembic.command
-import transaction
-from pyramid.paster import get_appsettings
-
-from leads_api import main
-from leads_api.models import get_tm_session, get_engine
-from leads_api.models.meta import metadata as dbmetadata
+from tests.integration import BaseIntegrationTest
 
 
-class CoverageGetTests(unittest.TestCase):
+class CoverageGetTests(BaseIntegrationTest):
 
-    def setUp(self):
-        settings = get_appsettings('testing.ini')
-        dbengine = get_engine(settings)
-        alembic_cfg = alembic.config.Config('testing.ini')
-
-        def dbcleanup():
-            dbmetadata.drop_all(bind=dbengine)
-            alembic.command.stamp(alembic_cfg, None, purge=True)
-
-        self.dbcleanup = dbcleanup
-        self.dbcleanup()
-        alembic.command.upgrade(alembic_cfg, "head")
-
-        app = main({}, dbengine=dbengine, **settings)
-        self.tm = transaction.manager
-        self.tm.begin()
-        self.tm.doom()
-        self.dbsession = get_tm_session(
-            app.registry['dbsession_factory'],
-            self.tm
-        )
-        self.testapp = TestApp(
-            app,
-            extra_environ={
-                'HTTP_HOST': 'leads_api.com',
-                'tm.active': True,
-                'tm.manager': self.tm,
-                'app.dbsession': self.dbsession,
-            },
-        )
-
-    def tearDown(self):
-        self.tm.abort()
-        self.dbcleanup()
-
-    def test_ok(self):
+    def make_one(self):
+        """Creates the needed data to run the tests"""
         from tests.integration.factories import (
             BuyerFactory,
             BuyerDealerFactory,
@@ -61,55 +16,76 @@ class CoverageGetTests(unittest.TestCase):
             set_session,
         )
 
+        # FakerBoy models needs to be associated with the db session
         set_session(self.dbsession)
 
-        make = MakeFactory()
-        buyer = BuyerFactory()
+        # Create a make and a buyer, and flush it so they are available for
+        # relations when creating the other models that include them as foreign keys
+        self.make = MakeFactory()
+        self.buyer = BuyerFactory()
         self.dbsession.flush()
-        buyer_dealer = BuyerDealerFactory(buyer_slug=buyer.slug)
-        buyer_tier = BuyerTierFactory(buyer_slug=buyer.slug)
-        buyer_make = BuyerMakeFactory(
-            buyer_slug=buyer.slug,
-            make_slug=make.slug,
+
+        # Fill the other tables that depend on buyer and make
+        self.buyer_dealer = BuyerDealerFactory(buyer_slug=self.buyer.slug)
+        self.buyer_tier = BuyerTierFactory(buyer_slug=self.buyer.slug)
+        self.buyer_make = BuyerMakeFactory(
+            buyer_slug=self.buyer.slug,
+            make_slug=self.make.slug,
         )
-        buyer_tier_make = BuyerTierMakeFactory(
-            buyer_slug=buyer.slug,
-            tier_slug=buyer_tier.slug,
-            make_slug=make.slug,
+        self.buyer_tier_make = BuyerTierMakeFactory(
+            buyer_slug=self.buyer.slug,
+            tier_slug=self.buyer_tier.slug,
+            make_slug=self.make.slug,
         )
-        dealer_coverage = BuyerDealerCoverageFactory(
-            buyer_slug=buyer.slug,
-            buyer_dealer_code=buyer_dealer.code
+        self.dealer_coverage = BuyerDealerCoverageFactory(
+            buyer_slug=self.buyer.slug,
+            buyer_dealer_code=self.buyer_dealer.code
         )
 
+    def test_ok(self):
+        """Test base coverage example"""
+
+        # Creates dummy data
+        self.make_one()
+
+        # Sets requested params
         params = {
-            'buyer_tier': buyer_tier.slug,
-            'make': make.slug,
-            'zipcode': dealer_coverage.zipcode,
+            'buyer_tier': self.buyer_tier.slug,
+            'make': self.make.slug,
+            'zipcode': self.dealer_coverage.zipcode,
         }
 
+        # Performs the request for coverage
         response = self.testapp.get('/v1/coverage', params=params)
 
+        # Should return OK status
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json.get('status'), 'ok')
+
+        # Should include metadata with params
         metadata = response.json.get('metadata')
         self.assertIsInstance(metadata, dict)
         self.assertEqual(metadata.get('params'), params)
+
+        # Should include the data with `has_coverage` flag and buyer info
         data = response.json.get('data')
         self.assertTrue(data.get('has_coverage'))
-        self.assertEqual(data.get('buyer'), buyer.name)
-        self.assertEqual(data.get('buyer_tier'), buyer_tier.name)
+        self.assertEqual(data.get('buyer'), self.buyer.name)
+        self.assertEqual(data.get('buyer_tier'), self.buyer_tier.name)
+
+        # Should include `coverage` as a list with a single element (for this case)
         coverage = data.get('coverage')
         self.assertIsInstance(coverage, list)
         self.assertEqual(len(coverage), 1)
         row = coverage[0]
 
-        self.assertEqual(row.get('dealer_code'), buyer_dealer.code)
-        self.assertEqual(row.get('dealer_name'), buyer_dealer.name)
-        self.assertEqual(row.get('dealer_address'), buyer_dealer.address)
-        self.assertEqual(row.get('dealer_city'), buyer_dealer.city)
-        self.assertEqual(row.get('dealer_state'), buyer_dealer.state)
-        self.assertEqual(row.get('dealer_zipcode'), buyer_dealer.zipcode)
-        self.assertEqual(row.get('dealer_phone'), buyer_dealer.phone)
-        self.assertEqual(row.get('distance'), dealer_coverage.distance)
-        self.assertEqual(row.get('zipcode'), dealer_coverage.zipcode)
+        # Coverage row should have the dealer and coverage info
+        self.assertEqual(row.get('dealer_code'), self.buyer_dealer.code)
+        self.assertEqual(row.get('dealer_name'), self.buyer_dealer.name)
+        self.assertEqual(row.get('dealer_address'), self.buyer_dealer.address)
+        self.assertEqual(row.get('dealer_city'), self.buyer_dealer.city)
+        self.assertEqual(row.get('dealer_state'), self.buyer_dealer.state)
+        self.assertEqual(row.get('dealer_zipcode'), self.buyer_dealer.zipcode)
+        self.assertEqual(row.get('dealer_phone'), self.buyer_dealer.phone)
+        self.assertEqual(row.get('distance'), self.dealer_coverage.distance)
+        self.assertEqual(row.get('zipcode'), self.dealer_coverage.zipcode)
