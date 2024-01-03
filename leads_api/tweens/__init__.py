3da2ed8b79f9c3afba95b2786eb272c4a8d61fc6
Pablo Diaz Ogni <pdiazogni-contractor@cars.com>
2024-01-03 13:16:49 -0300

    Fixed tween only for json responses

--- leads_api/tweens/__init__.py
+++ leads_api/tweens/__init__.py
@@ -12,22 +12,25 @@ def base_response_tween(handler, registry):
         # Handle the request
         response = handler(request)
 
-        former_response = response.json
-        json_response = {
-            'metadata': {
-                'params': dict(request.params),
-            },
-        }
+        # For any json response add metadata
+        if response.content_type == 'application/json':
+            former_response = response.json
+            json_response = {
+                'metadata': {
+                    'params': dict(request.params),
+                },
+            }
 
-        # If the request had an error, we want to return a document specifying
-        # the list of errors
-        if response.status_code < 400:
-            json_response['data'] = former_response
-        else:
-            json_response['errors'] = former_response
+            # If the request had an error, we want to return a document specifying
+            # the list of errors
+            if response.status_code < 400:
+                json_response['data'] = former_response
+            else:
+                json_response['errors'] = former_response
 
-        # Inject wrapped response
-        response.json = json_response
+            # Inject wrapped response
+            response.json = json_response
+
         return response
     return wrapper
 
